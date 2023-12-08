db13d09bef12914279c0f70352b4e32bb735ba71
Amila T Kumarasekara <84329334+amila-t-kumarasekara@users.noreply.github.com>
2023-12-08 15:25:24 +0530

    change port

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use(cors(corsOptions));
 
 app.use(hotelRoutes.routes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
